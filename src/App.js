4a3a81674dbe1d9c84f2aef5b42595a499622ec7
Ju111 <SchneiderJu@gmx.net>
2018-02-22 15:25:48 +0100

    players added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Title from './components/Title';
 
+const players = [
+  { id: 1, name: 'Arno', score: 4 },
+  { id: 2, name: 'Mat', score: 6 },
+  { id: 3, name: 'Mike', score: 3 },
+  { id: 4, name: 'Wouter', score: 4 },
+  { id: 5, name: 'Bram', score: 5 },
+  { id: 6, name: 'Mimi', score: 3 }
+]
+
 class App extends Component {
   render() {
     return (
-        <div>
+        <div className="App">
           <Title content="Scoreboard" />
         </div>
     );
